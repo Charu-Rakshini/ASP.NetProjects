0b8119e17a4c867cb8677538e8d93d7dcae2138a
Charu_Vijay <raks.charu@yahoo.com>
2021-04-22 17:45:16 -0400

    Initial Commit

---
+++ ToDoWebsite/Data/ToDoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ToDoWebsite.Models;
+
+namespace ToDoWebsite.Data
+{
+    public class ToDoDbContext : IdentityDbContext
+    {
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ToDoModel> ToDos { get; set; }
+    }
+}
---
+++ ToDoWebsite/Models/ToDoModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoWebsite.Models
+{
+    public class ToDoModel
+    {
+        [Key]
+        [HiddenInput]
+        public int ToDoModelId { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Please provide a valid email address")]
+        public String UserEmail { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public String Title { get; set; }
+
+        [StringLength(1000)]
+        public String Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? AddedDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        public Boolean Done { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DoneDate { get; set; }
+
+
+        public ToDoModel()
+        {
+        }
+    }
+}
+
