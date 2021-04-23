f8e71db502c2af31c742fefbcc3662b91627badc
Charu_Vijay <raks.charu@yahoo.com>
2021-04-23 02:41:34 -0400

    Added Tasks page and Filtered content in Index

--- ToDoWebsite/Pages/Index.cshtml.cs
+++ ToDoWebsite/Pages/Index.cshtml.cs
@@ -5,21 +5,50 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
+using ToDoWebsite.Data;
 
 namespace ToDoWebsite.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ToDoDbContext _context;
+
+        public IndexModel(ILogger<IndexModel> logger, ToDoDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
+
+        [BindProperty]
+        public Models.ToDoModel ToDo { get; set; }
+
+        //public ICollection<Models.ToDoModel> Temp;
+        public ICollection<Models.ToDoModel> Done;
+        public ICollection<Models.ToDoModel> Next;
+
+        //ICollection<Models.ToDoModel> Done = new ICollection<Models.ToDoModel>();
+        //List<Models.ToDoModel> Next = new List<Models.ToDoModel>();
+
 
         public void OnGet()
         {
+            //Temp = _context.ToDos.Where(todo => todo.DueDate < DateTime.Now).ToList();
+            TimeSpan hoursOfDay = new TimeSpan(24, 0, 0);
+            //var timeSinceOrder = DateTime.Now - Temp.FirstOrDefault<DueDate>;
 
+            //if (timeSinceOrder < TimeSpan.FromHours(24))
+            //{
+            //Done = _context.ToDos.Where(todo => TimeSpan.Compare(DateTime.Now.Hour,todo.DueDate.TimeOfDay.Hours) < TimeSpan.FromHours(24)).ToList();
+            Done = _context.ToDos.Where(todo => todo.DoneDate == DateTime.Now.Date).ToList();
+            //}
+
+
+
+            Next = _context.ToDos.Where(todo => todo.DueDate == DateTime.Now.Date.AddDays(1)).ToList();
         }
     }
 }
---
+++ ToDoWebsite/Pages/Tasks.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToDoWebsite.Data;
+
+
+namespace ToDoWebsite.Pages
+{
+    public class TasksModel : PageModel
+    {
+        private readonly ToDoDbContext _context;
+
+        public TasksModel(ToDoDbContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Models.ToDoModel> TaskList;
+
+        public void OnPost(int? ToDoModelId)
+        {
+            Models.ToDoModel task = _context.ToDos.FirstOrDefault(t => t.ToDoModelId == ToDoModelId);
+            task.Done = true;
+            _context.Update(task);
+            _context.SaveChanges();
+            Redirect("/index");
+        }
+        public void OnGet()
+        {
+            TaskList = _context.ToDos.ToList();
+        }
+    }
+}
