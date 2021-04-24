1c2ff7ebe54c06ac09409ab7b6adc0094d2b44ec
Charu_Vijay <raks.charu@yahoo.com>
2021-04-24 00:03:53 -0400

    ToDo Assignment - Pt2

--- ToDoWebsite/wwwroot/js/site.js
+++ ToDoWebsite/wwwroot/js/site.js
@@ -8,3 +8,40 @@
 
 
 
+function onCopy(taskName) {
+    alert("Trying to copy task");
+    
+    
+    document.getElementById("btnCopyTask").addEventListener("click", function () {
+        //var user_name = document.getElementById("#taskTitleText");
+        alert(taskName);
+        val=navigator.clipboard.write(new ClipboardItem(taskName));
+   //// var value = user_name.value.trim();
+   //     if (taskName)
+   //     alert("Task cannot be copied!");
+   // else {
+   //         navigator.clipboard.write(new ClipboardItem(taskName));
+   //     }
+
+    });
+}
+
+
+document.getElementById('status').innerHTML = navigator.onLine ? 'online' : 'offline';
+
+var target = document.getElementById('target');
+
+function handleStateChange() {
+    var timeBadge = new Date().toTimeString().split(' ')[0];
+    var newState = document.createElement('p');
+    var state = navigator.onLine ? 'online' : 'offline';
+    if (state = true) {
+        document.getElementById("createForm").style.display = "none";
+    } else {
+        document.getElementById("createForm").style.display = "block";
+    }
+    newState.innerHTML = '' + timeBadge + ' Connection state changed to ' + state + '.';
+    target.appendChild(newState);
+}
+
+window.addEventListener('online', handleStateChange);
